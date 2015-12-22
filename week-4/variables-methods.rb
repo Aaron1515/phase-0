ad7f552649cad52c6c5e703d73d68c9617e19266
Aaron Hu <aaron1515@gmail.com>
2015-12-22 13:18:04 -0800

    Update variables-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -28,12 +28,13 @@ How do you define a method?
 
 What is the difference between a local variable and a method?
   Local variable can be used anywhere locally on the file.  Method are defined variable by the programer.  Method could also contain local variables as well.
+
 How do you run a ruby program from the command line?
-  First you can start the ruby command line by typing "irb" and type your codes there and test them out.
-  Second you
+  First you can start the ruby command line by typing "irb" and type your codes there and test them out. That is mainly to test out simple codes.
+  Second you can also run a ruby file by typing "ruby [filename].rb" and run the file in command line.
 
 How do you run an RSpec file from the command line?
-  In command line you type "rspec [filename.rb]"  That will check the rspec to make sure you are running the code correctly.
+  In command line you type "rspec [filename.rb]"  That will check the rspec to make sure you are running the code correctly that matches with the rspec file.
 
 What was confusing about this material? What made sense?
   What was confusing was the rspec file on 4.3.1 when I was checking rspec to make sure I was doing it correctly.  After reading the rspec file I was able to find that their example vs the rspec file given was off.  After reading the rspec and matching up the example I was able to figure out that there was an issues with either the example or the rspec file.  So therefor I used the string in rspec to pass my rspec check.  Everything in this excercise made a lot of sense.  I love back end programing!!!! Sorry html and CSS.
