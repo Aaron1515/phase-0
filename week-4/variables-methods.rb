ed6593b22a79f621baa1666184cb47d74db66040
Aaron Hu <aaron1515@gmail.com>
2015-12-22 13:19:05 -0800

    Update variables-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -27,7 +27,7 @@ How do you define a method?
   To define a method you start by having "def" and next you will put the name of the method.  If there are parameter(s) you will put those in the "()" and seprate them with a comma.
 
 What is the difference between a local variable and a method?
-  Local variable can be used anywhere locally on the file.  Method are defined variable by the programer.  Method could also contain local variables as well.
+  Local variable can be used anywhere locally on the file.  Method are defined variable by the programer.  Method could also contain local variables as well. Methods could also contain parameter or parameters.
 
 How do you run a ruby program from the command line?
   First you can start the ruby command line by typing "irb" and type your codes there and test them out. That is mainly to test out simple codes.
