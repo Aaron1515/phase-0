db5e1dbecaa01463044aa2f2a60aa6475bed6f2e
Aaron Hu <aaron1515@gmail.com>
2016-01-04 11:53:37 -0800

    Update my_solution.rb

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -7,3 +7,4 @@ def make_address (street, city, state, zip)
   return "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
 end
 
+puts make_address("633 Folsom St.","San Francisco","CA","94107")
