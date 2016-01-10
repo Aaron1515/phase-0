47ba967ace7b616198b0093d86f8b9ddcc428f3e
Aaron Hu <aaron1515@gmail.com>
2016-01-10 05:01:22 -0800

    finish acct group

--- week-5/calculate-median/my_solution.rb
+++ week-5/calculate-median/my_solution.rb
@@ -4,12 +4,12 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [myself ].
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An array of objects
+# What is the output? The median of the highest and lowers of each , answer must be within the array.
 # What are the steps needed to solve the problem?
 
 
