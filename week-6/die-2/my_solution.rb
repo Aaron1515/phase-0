fffe3e147c63583602855c2982fd13a5eda87162
Aaron Hu <aaron1515@gmail.com>
2016-01-11 20:11:16 -0800

    crated empty gps

---
+++ week-6/die-2/my_solution.rb
@@ -0,0 +1,54 @@
+# Implement a Reverse Polish Notation Calculator
+
+
+# I worked on this challenge [by myself, with: ].
+
+# Pseudocode
+
+# Input: Range of object or an array of objects
+# Output: Take that array or range or object and display 1 random item from that array or range.
+# Steps:
+
+# Create a CLASS to perform this operation
+# Initialialize the the first method that except a object
+
+# Create a method for sides
+
+# Create a method for random roll once.
+
+
+# Initial Solution
+
+class Die
+
+  def initialize(labels)
+    if labels = 0
+      raise ArgumentError.new('I need more than an array that has something in it.')
+    end
+
+  @sides = lables
+  end
+
+  def sides
+    if @sides == 0
+      return 0
+    elsif
+    @sides.length
+    end
+  end
+
+  def roll
+    @sides.sample
+  end
+end
+
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# Reflection
