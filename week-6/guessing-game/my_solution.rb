19b7bc8551d259702955bb47f7b7a1d589274fe8
Aaron Hu <aaron1515@gmail.com>
2016-01-12 14:51:56 -0800

    finished guessing game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -39,62 +39,63 @@
 # Initial Solution
 
 #
-# Create a class called GuessingGame
+# class GuessingGame
+#   attr_reader :answer
+#   def initialize(answer)
+#     @answer = answer
+#   end
+#   def guess(guessing_number)
+#     if guessing_number < @answer
+#       @guessing_number = guessing_number
+#       :low
+#     elsif guessing_number > @answer
+#       @guessing_number = guessing_number
+#       :high
+#     elsif guessing_number == @answer
+#       @guess = guessing_number
+#       :correct
+#     end
+#   end
+
+#   def solved?
+#     @guessing_number == @answer
+#   end
+# #Driver Code - Display last guess number
+#   def last_guess
+#     @guessing_number
+#   end
+# end
+
+
+# Refactored Solution
+
 class GuessingGame
-  # Create a veriable called last answer
-  attr_accessor :guess
-  # Create a method called initialize
-# Set 1 CLASS variables
-#   First one is answer
+ attr_reader :answer
   def initialize(answer)
     @answer = answer
   end
-
-# Create a method called guess
   def guess(guessing_number)
-
-# The method should return higher or lower or correct.
-
-# If guessing number is higher than answer reply with Lower.
+    @guessing_number = guessing_number
     if guessing_number < @answer
       :low
-# Then store the guessing number
-
-# If guessing number  is lower than answer reply with higher.
     elsif guessing_number > @answer
       :high
-# Then store the guessing number
-
-# If guessing number is equal to answer reply with Correct.
     elsif guessing_number == @answer
       :correct
-# Then store the guessing number
-
-# End the if scenario
     end
   end
-
   def solved?
-# Create a method called solved?
-# If the answer is equal to the guessing number reply with Solved.
-    @guess == @answer
-# If not then answer it with NO it has not been solved.
-# End this method
-  # Make sure you define the other required methods, too
+    @guessing_number == @answer
   end
-#Driver Code - Display last guess number
   def last_guess
-    @guess
+    @guessing_number
   end
 end
 
 
 
-# Refactored Solution
-
-
-
-
-
 
 # Reflection
+=begin
+
+=end
