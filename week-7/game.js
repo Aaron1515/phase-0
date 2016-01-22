563f28035a03fd92ca1dcb1f6445315b45129a6f
Aaron Hu <aaron1515@gmail.com>
2016-01-22 14:42:27 -0800

    finshed pairing and adding reflections

---
+++ week-7/game.js
@@ -0,0 +1,57 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission: A game that punch and kicks.  Calling it Hood Fighter.  You have to fight through people in the hood to be the winner of the drug lord in the hood.
+// Goals: Have a few commands like walk, kick, punch, and runaway.
+// Characters: Main character is Jerome.
+// Objects: To be the king of the hood.
+// Functions: Walk, Kick, Punch, Leave the hood.
+
+// Pseudocode
+//Starting Charters
+Player: name:  - ??????
+        age: ????
+        health - 100%
+
+Enemy: name: through
+        age: 16
+        health: 100%
+
+
+ablity: punch: reduce health
+        kick: reduce health
+        leave: leave the game
+
+What is player name?
+What is player age?
+
+//
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
