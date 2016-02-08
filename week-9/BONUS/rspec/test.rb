21bf6a15a3cef3a0f043df7b178abc0c474eccf5
Aaron Hu <aaron1515@gmail.com>
2016-02-07 19:05:43 -0800

    started on bonus materials

---
+++ week-9/BONUS/rspec/test.rb
@@ -0,0 +1,20 @@
+# Creating Class called RomanNumberConverter
+class RomanNumeralsConverter
+
+# Create a method called roman to numer
+def roman_to_number(roman)
+  if
+    roman == "I"
+    return 1
+  elsif
+    roman == "II"
+    return 2
+  end
+
+end
+
+
+
+
+
+end
---
+++ week-9/CarClass/CarClass.rb
@@ -0,0 +1,51 @@
+# Create a Car Class from User Stories
+
+
+# I worked on this challenge [by myself, with: ].
+
+
+# 2. Pseudocode
+# Car Clas
+# As a video game player, I want to be able to create a new car and set its model and color so I can customize it in the video game.
+# As a video game player, I need to be able to define a distance to drive so I can follow directions appropriately.
+# As a video game player, I'd like to be able to see the speed I am traveling, so I can properly accelerate or decelerate.
+# As a video game player, I want to be able to turn left or right so I can navigate a city and follow directions.
+# As a video game player, I want to be able to accelerate and decelerate the car to a defined speed so I can drive following the rules of the road.
+# As a video game player, I want to keep track of the total distance I have travelled, so I can get paid for mileage.
+# As a video game player, I want to be able to stop the car so I can follow traffic signs and signals.
+# As a video game player, I would like to see output reflecting the latest action of my car so I can see a play-by-play of the pizza delivery.
+
+
+# 3. Initial Solution
+
+class Car
+  def initilize
+  end
+
+  def speed
+  end
+
+  def move(direction)
+  end
+
+  def miles
+  end
+
+end
+
+
+# 4. Refactored Solution
+
+
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+
+
+
+# 5. Reflection
